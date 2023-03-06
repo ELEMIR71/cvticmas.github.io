023fd0e51efe28f54dd92514a764f6fc0aa7df59
Emir <124746082+ELEMIR71@users.noreply.github.com>
2023-03-05 21:07:29 -0300

    init commit

---
+++ function.js
@@ -0,0 +1,48 @@
+function showname(){
+    document.getElementById("name").style.display = "inline-flex";
+    document.getElementById("info").style.backgroundColor = "rgb(98, 225, 213)";
+   
+         };
+  function ocultaname(){
+    document.getElementById("name").style.display = "none";
+    document.getElementById("info").style.backgroundColor = "" ;
+   
+  };
+function showcorreo(){
+  document.getElementById("correo").style.display = "inline-flex";
+  document.getElementById("info").style.backgroundColor = "rgb(98, 225, 213)";
+}
+function ocultacorreo(){
+  document.getElementById("correo").style.display = "none";
+  document.getElementById("info").style.backgroundColor = "" ;
+}
+function showfecha(){
+  document.getElementById("fecha").style.display = "inline-flex";
+  document.getElementById("info").style.backgroundColor = "rgb(98, 225, 213)";
+ 
+       };
+function ocultafecha(){
+  document.getElementById("fecha").style.display = "none";
+  document.getElementById("info").style.backgroundColor = "" ;
+ 
+};
+function showvivo(){
+  document.getElementById("vivo").style.display = "inline-flex";
+  document.getElementById("info").style.backgroundColor = "rgb(98, 225, 213)";
+ 
+       };
+function ocultavivo(){
+  document.getElementById("vivo").style.display = "none";
+  document.getElementById("info").style.backgroundColor = "" ;
+ 
+};
+function showtele(){
+  document.getElementById("tele").style.display = "inline-flex";
+  document.getElementById("info").style.backgroundColor = "rgb(98, 225, 213)";
+ 
+       };
+function ocultatele(){
+  document.getElementById("tele").style.display = "none";
+  document.getElementById("info").style.backgroundColor = "" ;
+ 
+};
